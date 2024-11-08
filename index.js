ac1f003a2609f46c3ff1c17e5d15a82cf881f7aa
Citren0 <cameron.davis1944@gmail.com>
2024-11-08 15:32:30 -0700

    Changed from node https to nginx.

--- index.js
+++ index.js
@@ -8,15 +8,10 @@ const rateLimit = require('express-rate-limit');
 
 // Servers.
 var http = require('http');
-var https = require('https');
-
-// Read TLS certificates.
-const fs = require('fs');
 
 // Express JS.
 var subdomain = require('express-subdomain');
 const express = require('express');
-var credentials = {key: privateKey, cert: certificate};
 
 // ENV file.
 require('dotenv').config();
@@ -74,8 +69,6 @@ app.use(subdomain('blog', routerBlog));
 
 // Middleware.
 app.set('view engine', 'ejs');
-//app.use(helmet());
-//app.use(hpp());
 const limiter = rateLimit({ windowMs: 60 * 1000, max: 100 });
 app.use(limiter);
 
@@ -106,24 +99,7 @@ app.get('/resume', (req, res, next) =>
 
 
 
-// HTTP or HTTPS Servers.
-if (process.env.SERVER_TYPE == 'production')
-{
-    var privateKey  = fs.readFileSync('sslcert/privkey.pem', 'utf8');
-    var certificate = fs.readFileSync('sslcert/fullchain.pem', 'utf8');
-    var caBundle = fs.readFileSync('sslcert/ca.bundle', 'utf8');
-    var credentials = {key: privateKey, cert: certificate, ca: caBundle};
-    var httpsPort = 443;
-
-    var httpsServer = https.createServer(credentials, app);
-    httpsServer.listen(httpsPort);
-    console.log(`Server is listening on port ${httpsPort}`);
-}
-else if (process.env.SERVER_TYPE == 'development')
-{
-    var httpPort = 80;
-
-    var httpServer = http.createServer(app);
-    httpServer.listen(httpPort);
-    console.log(`Server is listening on port ${httpPort}`);
-}
+var httpPort = 80;
+var httpServer = http.createServer(app);
+httpServer.listen(httpPort);
+console.log(`Server is listening on port ${httpPort}`);
